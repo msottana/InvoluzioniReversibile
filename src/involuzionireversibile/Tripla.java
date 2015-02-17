0971dbb99e86ad9ddb80333c54c40d7d19360c2b
gcorsi <giuliacorsi@outlook.it>
2015-02-17 10:56:03 +0100

    Aggiunta clase Tripla e tolti i sout di debug

---
+++ src/involuzionireversibile/Tripla.java
@@ -0,0 +1,15 @@
+/*
+ * Questa classe rappresenta la struttura dati utilizzate per rappresentare 
+ * tutte le informazioni necessarie a rappresentare una catena di Makov
+ */
+package involuzionireversibile;
+
+/**
+ *
+ * @author Giulia & Matteo
+ */
+public class Tripla {
+    double pi[];
+    double chain[][];
+    int ro[];
+}
